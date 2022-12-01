547306fffcd8971753e69c13660a2654315cd34d
Dominik Mydlil <dominik.mydlil@outlook.com>
2022-12-01 09:53:22 +0100

    day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,4 +1,38 @@
+pub fn sums(input: &str) -> Vec<u32> {
+    dbg!(input);
+    let blocks: Vec<Vec<u32>> = input
+        .split("\n\n")
+        .map(|b| b.lines().map(|l| l.parse().unwrap()).collect())
+        .collect();
+
+    let mut sums: Vec<_> = blocks.iter().map(|b| b.iter().sum::<u32>()).collect();
+    sums.sort();
+    sums
+}
+
 #[allow(dead_code)]
-pub fn run() {
-    // let input = include_str!("../input/01.txt");
+pub fn one(input: &str) -> u32 {
+    *sums(input).iter().rev().next().unwrap()
+}
+
+#[allow(dead_code)]
+pub fn two(input: &str) -> u32 {
+    sums(input).iter().rev().take(3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = "src/input/01.txt";
+
+    #[test]
+    fn one() {
+        let input = std::fs::read_to_string(INPUT).unwrap();
+        assert_eq!(69281, super::one(&input));
+    }
+
+    #[test]
+    fn two() {
+        let input = std::fs::read_to_string(INPUT).unwrap();
+        assert_eq!(201524, super::two(&input));
+    }
 }
