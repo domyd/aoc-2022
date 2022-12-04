100565750439f8fda93367c83d6dcfa23671a75b
Dominik Mydlil <dominik.mydlil@outlook.com>
2022-12-04 13:48:36 +0100

    support both parts in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::env;
+
 mod days;
 mod utils;
 
 fn main() {
-    let input = include_str!("input/03.txt");
-    days::day03::part2(input);
+    let args: Vec<String> = env::args().collect();
+    let input = std::fs::read_to_string("src/input/example.txt").unwrap();
+    let result = match args.into_iter().skip(1).next().map(|n| n.parse::<u32>()) {
+        Some(Ok(1)) => days::day04::one(&input),
+        Some(Ok(2)) => days::day04::two(&input),
+        _ => {
+            eprintln!("Must pass either `1` or `2` as first argument.");
+            return;
+        }
+    };
+
+    eprintln!("{}", result);
 }
