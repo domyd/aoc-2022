db02f8ab35537ab4c1877e97041a2b0f88987255
Dominik Mydlil <dominik.mydlil@outlook.com>
2022-12-08 12:04:56 +0100

    turn grid helper methods into a proper module

--- src/utils.rs
+++ src/utils.rs
@@ -1,48 +1,184 @@
-use std::{collections::HashMap, fmt::Display};
-
 #[allow(dead_code)]
-pub fn print_hashmap_grid<V: Display>(map: &HashMap<(isize, isize), V>) {
-    if map.is_empty() {
-        eprintln!("map is empty");
-        return;
+pub mod grid {
+    use std::{
+        collections::HashMap,
+        fmt::Display,
+        ops::{Add, Mul},
+    };
+
+    #[derive(Clone, Debug)]
+    pub struct Grid<V> {
+        pub map: HashMap<Point2, V>,
+        pub width: usize,
+        pub height: usize,
     }
 
-    let keys: Vec<_> = map.keys().collect();
-    let min_y = keys.iter().min_by_key(|f| f.0).map(|f| f.0).unwrap();
-    let min_x = keys.iter().min_by_key(|f| f.1).map(|f| f.1).unwrap();
-    let max_y = keys.iter().max_by_key(|f| f.0).map(|f| f.0).unwrap() + 1;
-    let max_x = keys.iter().max_by_key(|f| f.1).map(|f| f.1).unwrap() + 1;
-    let longest_v = map.values().map(|v| v.to_string().len()).max().unwrap();
-    let empty = core::iter::repeat(' ').take(longest_v).collect::<String>();
-
-    let mut out = String::new();
-    for y in min_y..max_y {
-        let row: Vec<_> = (min_x..max_x)
-            .map(|x| map.get(&(y, x)).map(|v| v.to_string()))
-            .collect();
-
-        for el in row.iter() {
-            let str = if let Some(s) = el { &s } else { &empty };
-            out.push_str(format!("{:>width$} ", str, width = longest_v).as_ref());
-        }
-
-        out.push('\n');
+    #[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Debug)]
+    pub struct Point2 {
+        pub x: isize,
+        pub y: isize,
     }
 
-    eprintln!("{}", out);
-}
+    impl Add<Point2> for Point2 {
+        type Output = Point2;
 
-#[allow(dead_code)]
-pub fn vec_into_hashmap<V: Copy>(vec: Vec<Vec<V>>) -> (HashMap<(isize, isize), V>, usize, usize) {
-    let rows = vec.len();
-    let cols = vec.iter().map(|l| l.len()).max().unwrap_or_default();
-
-    let mut map = HashMap::new();
-    for y in 0..rows {
-        for x in 0..cols {
-            map.insert((y as isize, x as isize), vec[y][x]);
+        fn add(self, rhs: Point2) -> Self::Output {
+            Point2 {
+                x: self.x + rhs.x,
+                y: self.y + rhs.y,
+            }
         }
     }
 
-    (map, rows, cols)
+    impl Mul<isize> for Point2 {
+        type Output = Point2;
+
+        fn mul(self, rhs: isize) -> Self::Output {
+            Point2 {
+                x: self.x * rhs,
+                y: self.y * rhs,
+            }
+        }
+    }
+
+    impl Mul<i32> for Point2 {
+        type Output = Point2;
+
+        fn mul(self, rhs: i32) -> Self::Output {
+            self * (rhs as isize)
+        }
+    }
+
+    #[derive(Clone, Copy)]
+    pub enum Direction {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest,
+    }
+
+    impl Direction {
+        pub fn cardinals() -> [Direction; 4] {
+            [
+                Direction::North,
+                Direction::East,
+                Direction::South,
+                Direction::West,
+            ]
+        }
+
+        pub fn ordinals() -> [Direction; 4] {
+            [
+                Direction::NorthEast,
+                Direction::SouthEast,
+                Direction::SouthWest,
+                Direction::NorthWest,
+            ]
+        }
+
+        pub fn all() -> [Direction; 8] {
+            [
+                Direction::North,
+                Direction::NorthEast,
+                Direction::East,
+                Direction::SouthEast,
+                Direction::South,
+                Direction::SouthWest,
+                Direction::West,
+                Direction::NorthWest,
+            ]
+        }
+
+        pub fn offset(&self) -> Point2 {
+            let (x, y) = match self {
+                Direction::North => (-1, 0),
+                Direction::East => (0, 1),
+                Direction::South => (1, 0),
+                Direction::West => (0, -1),
+                Direction::NorthEast => (1, 1),
+                Direction::SouthEast => (1, -1),
+                Direction::SouthWest => (-1, -1),
+                Direction::NorthWest => (-1, 1),
+            };
+            Point2 { x, y }
+        }
+    }
+
+    impl<V: Copy> Grid<V> {
+        pub fn from_vec(vec: Vec<Vec<V>>) -> Self {
+            let rows = vec.len();
+            let cols = vec.iter().map(|l| l.len()).max().unwrap_or_default();
+
+            let mut map = HashMap::with_capacity(rows * cols);
+            for y in 0..rows {
+                for x in 0..cols {
+                    map.insert(
+                        Point2 {
+                            x: x as isize,
+                            y: y as isize,
+                        },
+                        vec[y][x],
+                    );
+                }
+            }
+
+            Self {
+                map,
+                height: rows,
+                width: cols,
+            }
+        }
+
+        pub fn line_starting_from<'a>(
+            &'a self,
+            p: Point2,
+            dir: Direction,
+        ) -> impl Iterator<Item = &V> + 'a {
+            let offset = dir.offset();
+            (0..)
+                .into_iter()
+                .map(move |n| p + (offset * n))
+                .map(|coord| self.map.get(&coord))
+                .take_while(|x| x.is_some())
+                .map(|x| x.unwrap())
+        }
+    }
+
+    impl<V: Display> Display for Grid<V> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let map = &self.map;
+            if map.is_empty() {
+                writeln!(f, "map is empty")?;
+                return Ok(());
+            }
+
+            let keys: Vec<_> = map.keys().collect();
+            let min_y = keys.iter().min_by_key(|f| f.x).map(|f| f.x).unwrap();
+            let min_x = keys.iter().min_by_key(|f| f.y).map(|f| f.y).unwrap();
+            let max_y = keys.iter().max_by_key(|f| f.x).map(|f| f.x).unwrap() + 1;
+            let max_x = keys.iter().max_by_key(|f| f.y).map(|f| f.y).unwrap() + 1;
+            let longest_v = map.values().map(|v| v.to_string().len()).max().unwrap();
+            let empty = core::iter::repeat(' ').take(longest_v).collect::<String>();
+
+            for y in min_y..max_y {
+                let mut out = String::new();
+                let row: Vec<_> = (min_x..max_x)
+                    .map(|x| map.get(&Point2 { x, y }).map(|v| v.to_string()))
+                    .collect();
+
+                for el in row.iter() {
+                    let str = if let Some(s) = el { &s } else { &empty };
+                    out.push_str(format!("{:>width$} ", str, width = longest_v).as_ref());
+                }
+
+                writeln!(f, "{}", out)?;
+            }
+
+            Ok(())
+        }
+    }
 }
