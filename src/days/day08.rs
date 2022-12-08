b6d42a48891f9ae58b365bff1898d35c356e6268
Dominik Mydlil <dominik.mydlil@outlook.com>
2022-12-08 12:05:03 +0100

    day 8

---
+++ src/days/day08.rs
@@ -0,0 +1,72 @@
+use crate::utils::grid::{Direction, Grid, Point2};
+
+#[allow(dead_code)]
+pub fn one(input: &str) -> u32 {
+    let grid = parse_grid(input);
+    grid.map
+        .keys()
+        .map(|p| is_visible(*p, &grid))
+        .filter(|b| *b)
+        .count() as u32
+}
+
+#[allow(dead_code)]
+pub fn two(input: &str) -> u32 {
+    let grid = parse_grid(input);
+    grid.map
+        .keys()
+        .map(|p| scenic_score(*p, &grid))
+        .max()
+        .unwrap()
+}
+
+fn scenic_score(p: Point2, grid: &Grid<u32>) -> u32 {
+    let height = grid.map.get(&p).unwrap();
+    Direction::cardinals()
+        .map(|d| {
+            let mut cnt = 0;
+            for tree_height in grid.line_starting_from(p, d).skip(1).copied() {
+                cnt += 1;
+                if tree_height >= *height {
+                    break;
+                }
+            }
+            cnt
+        })
+        .into_iter()
+        .fold(1, |acc, x| acc * x)
+}
+
+fn is_visible(p: Point2, grid: &Grid<u32>) -> bool {
+    let height = grid.map.get(&p).unwrap();
+    Direction::cardinals()
+        .map(|d| grid.line_starting_from(p, d).skip(1).all(|n| n < height))
+        .into_iter()
+        .any(|b| b)
+}
+
+fn parse_grid(input: &str) -> Grid<u32> {
+    Grid::from_vec(
+        input
+            .lines()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .collect(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = "src/input/08.txt";
+
+    #[test]
+    fn one() {
+        let input = std::fs::read_to_string(INPUT).unwrap();
+        assert_eq!(1717, super::one(&input));
+    }
+
+    #[test]
+    fn two() {
+        let input = std::fs::read_to_string(INPUT).unwrap();
+        assert_eq!(321975, super::two(&input));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let input = std::fs::read_to_string("src/input/example.txt").unwrap();
     let result = match args.into_iter().skip(1).next().map(|n| n.parse::<u32>()) {
-        Some(Ok(1)) => days::day07::one(&input),
-        Some(Ok(2)) => days::day07::two(&input),
+        Some(Ok(1)) => days::day08::one(&input),
+        Some(Ok(2)) => days::day08::two(&input),
         _ => {
             eprintln!("Must pass either `1` or `2` as first argument.");
             return;
